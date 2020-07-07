22c7b27fe3bbb9a93744506216a3a2d3927017ed
Alvaro Fabian <alvarofabian0432@gmail.com>
2020-07-07 01:36:18 -0500

    CRUD para autos funcional

--- controllers/AutoController.js
+++ controllers/AutoController.js
@@ -10,11 +10,85 @@ autoController.list = function (req,res) {
             return;
         }
         res.render('../views/autom/index',{autos: autos})
-    })
+    });
 };
 
 autoController.create = function (req,res) {
     res.render('../views/autom/create');
 };
+
+autoController.save = function (req,res) {
+    var autom = new Autom( req.body );
+
+    autom.save(function(err) {
+        if (err) {
+            console.log('Error', err);
+            return;
+        }
+        console.log("Auto registrado satisfactoriamente");
+        res.redirect("/autos/show/"+autom._id);
+        //res.redirect("/autos");
+    });
+};
+
+
+autoController.show = function (req,res) {
+
+    Autom.findOne({_id: req.params.id }).exec(function(err,autom) {
+        if (err) {
+            console.log('Error', err);
+            return;
+        }
+        res.render('../views/autom/show',{autom: autom})
+    });
+};
+
+autoController.edit = function (req,res) {
+
+    Autom.findOne({_id: req.params.id }).exec(function(err,autom) {
+        if (err) {
+            console.log('Error', err);
+            return;
+        }
+        res.render('../views/autom/edit',{autom: autom})
+    });
+};
+
+autoController.update = function(req,res) {
+
+    Autom.findByIdAndUpdate( req.params.id, {$set: {
+        nombre: req.body.nombre,
+        marca: req.body.marca,
+        color: req.body.color,
+        estado: req.body.estado,
+        stock: req.body.stock,
+        capacidad: req.body.capacidad
+    }}, { new : true}, 
+        
+    function( err, autom) {
+        if( err ){
+            console.log('Error: ', err);
+            res.render('../views/autom/edit', {autom: req.body} );
+        }
+        
+        console.log( autom );
+
+        res.redirect('/autos/show/' + autom._id );
+    });
+};
+
+
+autoController.delete = function (req,res) {
+
+    Autom.remove( { _id: req.params.id} , function(err) {
+        if (err) {
+            console.log('Error', err);
+            return;
+        }
+        console.log("Auto eliminado")
+        res.redirect('/autos');
+    });
+};
+
 
 module.exports = autoController;
--- routes/autos.js
+++ routes/autos.js
@@ -5,5 +5,10 @@ var autom = require('../controllers/AutoController');
 
 router.get('/', autom.list);
 router.get('/create', autom.create);
+router.post('/save', autom.save);
+router.get('/show/:id', autom.show);
+router.get('/edit/:id', autom.edit);
+router.post('/update/:id', autom.update);
+router.post('/delete/:id', autom.delete);
 
 module.exports = router;
